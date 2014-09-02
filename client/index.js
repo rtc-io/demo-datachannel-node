a310ab0f8c8dac261102437f569f6573bad689b7
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-02 17:43:20 +1000

    Simplified client code

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 var quickconnect = require('rtc-quickconnect');
-var serverUrl = typeof location != 'undefined' ? location.origin : 'http://localhost:3000/';
-var qc = quickconnect(serverUrl, { room: 'test' });
+var qc = quickconnect(location.origin, { room: 'test' });
 
 qc.createDataChannel('chat').on('channel:opened:chat', function(id, dc) {
     console.log('channel opened with peer: ' + id);
